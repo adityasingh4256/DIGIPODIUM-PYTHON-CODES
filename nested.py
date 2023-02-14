7dfe68002efd772fca2a398f606ac04cef3aa9a9
adityasingh4256 <adityasingh4256@yahoo.com>
2023-02-14 20:01:17 +0530

    functions and nested loop

---
+++ nested.py
@@ -0,0 +1,23 @@
+from turtle import *
+speed('fastest')
+pencolor('orange')
+fillcolor('red')
+pensize(2)
+dot(10)
+side = 6
+for i in range(side):
+    fd(-50)
+    begin_fill()
+    for i in range(side):
+        fd(-50)
+        for i in range(side):
+            fd(50/2)
+            lt(360/side)
+            dot(10)
+            circle(-10)
+        rt(360/side)
+            
+    end_fill()
+    lt(360/side)
+hideturtle()
+mainloop()
